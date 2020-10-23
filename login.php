36fe65336808766b3001eb60d3e7e74fe3fff70d
leonelrm <69789534+leonelrm@users.noreply.github.com>
2020-10-23 13:07:25 +0200

    update 52

---
+++ login.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="public/css/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <form class="container">
+            <form class="card-title ">
+                <h1 class="text-center">Login Form</h1>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email </label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Mot de Passe</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+    </form>
+</div>
+
+</body>
+</html>
